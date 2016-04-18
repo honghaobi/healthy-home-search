e993196134b3e3b77d5b1c35e8990b79df167c3b
henrybi <henrybi@u.washington.edu>
2016-04-18 11:14:38 -0700

    added modules

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index');
 });
+
+router.post('/', function(req, res, next) {
+  models.getGeoCode(userSearch).then(function(data){
+    
+    res.render('result', {data});
+  })
+});
+
 
 module.exports = router;
