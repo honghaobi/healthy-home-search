4e28b0f766a5549b2ff867756b70d215e541d430
henrybi <henrybi@u.washington.edu>
2016-04-19 13:11:29 -0700

    added foundation and structured the page layout

---
+++ public/javascripts/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+
+	/*  Foundation Init    */
+	$(document).foundation();
+
+
+	/*    Mean navigation menu scroll to    */
+    $('#mean_nav ul li a').click(function(e){
+    	e.preventDefault();
+    	scrollTo($(this).attr('href'), 900, 'easeInOutCubic');
+    });
+
+		$('.score_arrow').click(function(e){
+			e.preventDefault();
+			scrollTo($(this).attr('href'), 900, 'easeInOutCubic');
+		});
+
+    /*    Back to top button    */
+    var back_top = $('#back_top');
+
+    back_top.click(function(e){
+    	e.preventDefault();
+    	scrollTo(0, 900, 'easeInOutCubic');
+
+    });
+
+    function scrollTo(target, speed, ease){
+    	$(window).scrollTo(target, speed, {easing:ease});
+    }
+
+    $(window).on('scroll', function(){
+	    if($(this).scrollTop()>749)
+	    {
+	    	back_top.stop().animate({opacity : 1}, 250);
+	    }else
+	    {
+	    	back_top.stop().animate({opacity : 0}, 250);
+	    }
+    });
+
+});
